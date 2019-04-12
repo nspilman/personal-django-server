f880eaf8eb3e3235d54b86c4738e266cb897aefa
Nate <nate.spilman@gmail.com>
2019-04-12 02:19:40 +0000

    Updates on serber?

--- mysite/static/js/index.js
+++ mysite/static/js/index.js
@@ -1,25 +1,55 @@
-console.log('you found me! Woohoo!')
-
 const event =  {
     template: `
-    <div class = "container eventCard card p-3">
-          <h1> {{event.name}}</h1>
-          <h4>{{date}}</h4>
+    <div class = "eventCard col-sm-4 card p-4">
+        <a :href="eventLink">
+            <div class = 'text-center'>
+                <img :src="imageUrl" class = "p-3"/>
+            </div>
+        </a>
+        <div class = "px-5">
+            <h4> 
+                {{event.name}}
+            </h4>
+            <h6>
+                {{date}}
+            </h6>
+            <h6 v-if="attendees">
+                attendees: {{attendees.length}}
+            </h6>
+            <h6 >
+                Host:
+                <a :href="'/frontend/person/'+event.created_user"> 
+                    {{event.created_user}}
+                </a>
+            </h6>
+        </div>
     </div>`,
     data(){
         return{
+            attendees:null,
+            imageUrl:"https://source.unsplash.com/300x200/?event/" + this.event.name,
+            eventLink: "/frontend/event/" + this.event.id
         }
     },
     computed:{
         date(){
             const {startdate, enddate} = this.event
-            console.log(startdate === enddate)
             if(startdate === enddate){
                 let date = startdate;
                 return new Date(date).toDateString()  
             } 
         }
     },
+        methods:{
+            async getAttendees(){
+              const resp = await axios.get(`/events/eventsignups/${this.event.id}`)
+              const data = await resp.data
+              this.attendees = data.attendees
+            }
+        },
+        created(){
+          this.getAttendees()  
+        },
     props:['event'],
     }
 
@@ -35,8 +65,7 @@ var app = new Vue({
        async getEvents(){
         const resp = await axios.get('/events');
         const data = await resp.data;
-        this.events = data
-        console.log(data) 
+        this.events = await data
         }
     },
     delimiters:['${','}'],
