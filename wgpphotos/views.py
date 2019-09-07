94080d783a10b641ca68b274e78823fa5866ff7f
Nate Spilman <Nate.spilman@gmail.com>
2019-09-07 09:34:42 -0700

    wgpphotos

--- wgpphotos/views.py
+++ wgpphotos/views.py
@@ -6,7 +6,25 @@ from rest_framework.response import Response
 
 class photos(APIView):
     def get(self, request):
-        return Response({"response":'whatsgood?'})
+        data = request.GET
+        category = data['category']
+
+        s3_client = boto3.client(
+            's3',
+            )
+        s3_resource = boto3.resource('s3')
+        bucket_name = 'website-photographs'
+        my_bucket = s3_resource.Bucket(bucket_name)
+
+        urls = []
+        for file in my_bucket.objects.all():
+            dir = file.key.split('/')[0]
+            if dir == category:
+                params = {'Bucket': bucket_name, 'Key': file.key}
+                url = s3_client.generate_presigned_url('get_object', params)
+                urls.append(url)
+
+        return Response({"response":urls})
 
     def post(self, request):
         data = request.data
