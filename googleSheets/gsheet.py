ec5fc395e3fb12e3bc7f28382f54764ad1a9afe2
Nate Spilman <nate.spilman@gmail.com>
2020-10-11 12:38:40 -0700

    fixing security issue

--- googleSheets/gsheet.py
+++ googleSheets/gsheet.py
@@ -5,10 +5,8 @@ import json
 from django.conf import settings
 
 creds = json.loads(os.environ.get('GSHEET_CONFIG'))
-with open('gcreds.json', 'w') as fp:
-    json.dump(creds, fp)
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('gcreds.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds, scope)
 client = gspread.authorize(creds)
 
