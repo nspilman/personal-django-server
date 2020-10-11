22946ddb5437cadd1e34cf080fe45bfe677f9b14
Nate Spilman <nate.spilman@gmail.com>
2020-10-11 09:07:08 -0700

    putting google sheet env variable access in settings.py

--- googleSheets/gsheet.py
+++ googleSheets/gsheet.py
@@ -2,19 +2,20 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
+from django.conf import settings
 
 def create_keyfile_dict():
     variables_keys = {
-        "type": os.environ.get("SHEET_TYPE","service_account"),
-        "project_id": os.environ.get("SHEET_PROJECT_ID","id"),
-        "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID","key_id"),
-        "private_key": os.environ.get("SHEET_PRIVATE_KEY","key"),
-        "client_email": os.environ.get("SHEET_CLIENT_EMAIL","email@address.com"),
-        "client_id": os.environ.get("SHEET_CLIENT_ID","sheet_id"),
-        "auth_uri": os.environ.get("SHEET_AUTH_URI","URI"),
-        "token_uri": os.environ.get("SHEET_TOKEN_URI","URI"),
-        "auth_provider_x509_cert_url": os.environ.get("SHEET_AUTH_PROVIDER_X509_CERT_URL","CertUrl"),
-        "client_x509_cert_url": os.environ.get("SHEET_CLIENT_X509_CERT_URL","CertUrl")
+        "type": os.environ.get(settings.SHEET_TYPE,"service_account"),
+        "project_id": os.environ.get(settings.SHEET_PROJECT_ID,"id"),
+        "private_key_id": os.environ.get(settings.SHEET_PRIVATE_KEY_ID,"key_id"),
+        "private_key": os.environ.get(settings.SHEET_PRIVATE_KEY,"key"),
+        "client_email": os.environ.get(settings.SHEET_CLIENT_EMAIL,"email@address.com"),
+        "client_id": os.environ.get(settings.SHEET_CLIENT_ID,"sheet_id"),
+        "auth_uri": os.environ.get(settings.SHEET_AUTH_URI,"URI"),
+        "token_uri": os.environ.get(settings.SHEET_TOKEN_URI,"URI"),
+        "auth_provider_x509_cert_url": os.environ.get(settings.SHEET_AUTH_PROVIDER_X509_CERT_URL,"CertUrl"),
+        "client_x509_cert_url": os.environ.get(settings.SHEET_CLIENT_X509_CERT_URL,"CertUrl")
     }
     return variables_keys
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -148,6 +148,18 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS',"http://localhost:8080").split(',')
 
+### Google Sheets settings
+SHEET_TYPE = os.environ.get("SHEET_TYPE","service_account")
+SHEET_PROJECT_ID= os.environ.get("SHEET_PROJECT_ID","id")
+SHEET_PRIVATE_KEY_ID = os.environ.get("SHEET_PRIVATE_KEY_ID","key_id")
+SHEET_PRIVATE_KEY = os.environ.get("SHEET_PRIVATE_KEY","key")
+SHEET_CLIENT_EMAIL = os.environ.get("SHEET_CLIENT_EMAIL","email@address.com")
+SHEET_CLIENT_ID = os.environ.get("SHEET_CLIENT_ID","sheet_id")
+SHEET_AUTH_URI = os.environ.get("SHEET_AUTH_URI","URI")
+SHEET_TOKEN_URI = os.environ.get("SHEET_TOKEN_URI","URI")
+SHEET_AUTH_PROVIDER_X509_CERT_URL = os.environ.get("SHEET_AUTH_PROVIDER_X509_CERT_URL","CertUrl")
+SHEET_CLIENT_X509_CERT_URL = os.environ.get("SHEET_CLIENT_X509_CERT_URL","CertUrl")
+
 try:
     import django_heroku
     django_heroku.settings(locals())
