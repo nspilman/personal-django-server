cb42a62c0c9ae440f98f4542117d4c6952a32e49
Nate Spilman <nate.spilman@gmail.com>
2020-10-20 08:02:45 -0700

    subscriptions app now as full CRUD

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -7,7 +7,11 @@ from . import views
 
 urlpatterns = [
     path('business/', views.Businesses.as_view()),
+    path('business/<int:pk>/', views.BusinessRecord.as_view()),
     path('offering/', views.Offerings.as_view()),
+    path('offering/<int:pk>/', views.OfferingRecord.as_view()),
     path('customer/', views.Customers.as_view()),
+    path('customer/<int:pk>/', views.CustomerRecord.as_view()),
     path('order/', views.Orders.as_view()),
+    path('order/<int:pk>/', views.OrderRecord.as_view()),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
@@ -20,6 +20,12 @@ def createNewRecord(request, Serializer):
         return JsonResponse(serializer.data, status=201)
     return JsonResponse(serializer.errors, status=400)
 
+def getRecord(classObject, pk):
+    try:
+        return classObject.objects.get(pk=pk)
+    except:
+        raise Http404
+
 # Create your views here.
 class Businesses(APIView):  
     permission_classes = [AllowAny]
@@ -29,6 +35,18 @@ class Businesses(APIView):
     def post(self,request):
         return createNewRecord(request, BusinessSerializer)
 
+class BusinessRecord(APIView):  
+    permission_classes = [AllowAny]
+    def get(self, request, pk, format=None):
+        business = getRecord(Business, pk)
+        serializer = BusinessSerializer(business)
+        return Response(serializer.data)
+    
+    def delete(self, request, pk, format=None):
+        record = getRecord(Business,pk)
+        record.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class Offerings(APIView):  
     permission_classes = [AllowAny]
     def get(self,request):
@@ -36,6 +54,18 @@ class Offerings(APIView):
     
     def post(self,request):
         return createNewRecord(request, BusinessOfferingSerializer)
+
+class OfferingRecord(APIView):  
+    permission_classes = [AllowAny]
+    def get(self, request, pk, format=None):
+        offering = getRecord(Business_Offering, pk)
+        serializer = BusinessOfferingSerializer(offering)
+        return Response(serializer.data)
+    
+    def delete(self, request, pk, format=None):
+        record = getRecord(Business_Offering,pk)
+        record.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class Customers(APIView):  
     permission_classes = [AllowAny]
@@ -45,6 +75,18 @@ class Customers(APIView):
     def post(self,request):
         return createNewRecord(request, CustomerSerializer)
 
+class CustomerRecord(APIView):  
+    permission_classes = [AllowAny]
+    def get(self, request, pk, format=None):
+        customer = getRecord(Customer, pk)
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+    
+    def delete(self, request, pk, format=None):
+        record = getRecord(Customer,pk)
+        record.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class Orders(APIView):  
     permission_classes = [AllowAny]
     def get(self,request):
@@ -52,3 +94,15 @@ class Orders(APIView):
     
     def post(self,request):
         return createNewRecord(request, OrderSerializer)
+    
+class OrderRecord(APIView):  
+    permission_classes = [AllowAny]
+    def get(self, request, pk, format=None):
+        order = getRecord(Order, pk)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+    
+    def delete(self, request, pk, format=None):
+        record = getRecord(Order,pk)
+        record.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
